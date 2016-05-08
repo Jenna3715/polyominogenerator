fe02bcbc8379651378412fedf65162d112149bd3
kartikvasu <kartiktv97@gmail.com>
2016-05-08 00:41:14 -0400

    Helper functions to main polyomino function

--- script.js
+++ script.js
@@ -5,4 +5,67 @@ array of Hexominos.
 */
 var returnPolyOminos = function (n) {
 
+	var numPolyomino = 0;
+	while(numPolyomino < n) {
+
+	}
+
+}
+
+/** Object constructor that creates a point and returns it.
+*/
+var Point = function(x, y) {
+	
+	this.x = x;
+	this.y = y;
+	return this;
+
+}
+
+/** Takes a list of points that represent a polyomino and 
+returns the array after performing a matrix rotation on the 
+set of points that are passed in. 
+*/
+var rotatePointsClockWise = function(pointArray) {
+
+	var i;
+
+	for(i = 0; i < pointArray.length; i++) {
+		pointArray[i] = Point(pointArray[i].y, -pointArray[i].x);
+	}
+
+	return pointArray;
+
+}
+
+/** Takes a list of points that represent a polyomino. Performs
+the matrix vertical flip operation on each one of the points and 
+returns an array of points. 
+*/
+var flipPointsVertically = function(pointArray) {
+	
+	var i;
+
+	for(i = 0; i < pointArray.length; i++) {
+		pointArray[i] = Point(-pointArray[i].x, pointArray[i].y);
+	}
+
+	return pointArray;
+
+}
+
+/** Takes a list of points that represent a polyomino. Performs
+the matrix horizontal flip operation on each one of the poitns and
+returns an array of points. 
+*/
+var flipPointsHorizontally = function(pointArray) {
+
+	var i;
+
+	for(i = 0; i < pointArray.length; i++) {
+		pointArray[i] = Point(pointArray[i].x, -pointArray[i].y);
+	}
+
+	return pointArray;
+
 }
