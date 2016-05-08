69c08d7c00e01c3a0554a5abc9bbe37b983f3543
kartikvasu <kartiktv97@gmail.com>
2016-05-07 23:58:34 -0400

    wrote skeleton script JS file

---
+++ script.js
@@ -0,0 +1,8 @@
+/** This is the main function that is called
+at the highest level. It calls several of the helper
+functions in the program in order to generate the desired
+array of Hexominos.
+*/
+var returnPolyOminos = function (n) {
+
+}
