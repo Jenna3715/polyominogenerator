438d5e81e699fa64be27a00ccb1faa07f8cc0285
Jenna Sloan <Jenna3715@users.noreply.github.com>
2018-07-28 00:54:06 -0500

    Various changes

--- JS/polyominoGenerator.js
+++ JS/polyominoGenerator.js
@@ -1,4 +1,4 @@
-polyominoGenerator = function() {
+polyominoGenerator = ((function() {
 /**----------------------------------------------------------
 				MODEL ELEMENT CONSTRUCTORS
 ------------------------------------------------------------*/
@@ -10,41 +10,30 @@ var Point = function(x, y) {
 		y: y
 	};
 }
-
 /** Object factory that creates a list of points that when
 together represent a piece. It returns a point array which is 
 an array of points. 
 */
 var Piece = function() {
-
 	var i;
-
 	//a piece has an array of points.
 	var pointArray = [];
-
 	for(i = 0; i < arguments.length; i += 2) {
 		pointArray[i/2] = Point(arguments[i], arguments[i+1]); 
 	}
-
 	return {
 		pointArray: pointArray
 	};
 }
-
  var PieceSpecial = function(array) {
-
  	var pointArray = [];
-
  	for(var i = 0; i < array.length; i += 2) {
  		pointArray.push(Point(array[i], array[i+1]));
  	}
-
  	return {
  		pointArray: pointArray
  	};
-
  }
-
 /*------------------------------------------------------------
 					ALGORITHMIC OPERATIONS
 ------------------------------------------------------------*/
@@ -54,66 +43,51 @@ functions in the program in order to generate the desired
 array of Hexominos.
 */
 this.returnPolyominos = function (num) {
-
-	if(typeof num != "number") {
-		console.log("For some reason what you entered is not a number.");
-		return;
-	}
-
-	return generatePolyominos(num);
+	return generatePolyominos(Number(num));
 }
-
 /** This is the actual function that calls around everything. 
 It is the "powerhouse of the cell" lol.
 It returns an array of pieces. 
 */
 var generatePolyominos = function(num) {
-	var start_time = Date.now();
+	//var start_time = Date.now();
 	//base case for the recursive function
 	if(num === 1) {
 		var pieceArray = [];
 		pieceArray[0] = Piece(0, 0);
-		var end_time = Date.now();
-		console.log(num, '-omino took: ', end_time - start_time, 'milliseconds');
+		//var end_time = Date.now();
+		//console.log(num, '-omino took: ', end_time - start_time, 'milliseconds');
 		return pieceArray; 
 	}
-
 	var newPolyominos = [],
 	prevPolyominos = generatePolyominos(num - 1); //recursive call here!!
-	
 	for(var i = 0; i < prevPolyominos.length; i++) {
 		var testPolyominos = [];
 		testPolyominos.push(copyPolyomino(prevPolyominos[i]));
 		testPolyominos.push(rotatePointsClockWise(copyPolyomino(prevPolyominos[i])));
 		testPolyominos.push(flipPointsVertically(rotatePointsClockWise(copyPolyomino(prevPolyominos[i]))));
 		testPolyominos.push(flipPointsHorizontally(rotatePointsClockWise(copyPolyomino(prevPolyominos[i]))));
-		
 		testPolyominos.push(rotatePointsClockWise(rotatePointsClockWise(copyPolyomino(prevPolyominos[i]))));
 		testPolyominos.push(flipPointsVertically(rotatePointsClockWise(rotatePointsClockWise(copyPolyomino(prevPolyominos[i])))));
 		testPolyominos.push(flipPointsHorizontally(rotatePointsClockWise(rotatePointsClockWise(copyPolyomino(prevPolyominos[i])))));
-
 		testPolyominos.push(rotatePointsClockWise(rotatePointsClockWise(rotatePointsClockWise(copyPolyomino(prevPolyominos[i])))));
 		testPolyominos.push(flipPointsVertically(rotatePointsClockWise(rotatePointsClockWise(rotatePointsClockWise(copyPolyomino(prevPolyominos[i]))))));
 		testPolyominos.push(flipPointsHorizontally(rotatePointsClockWise(rotatePointsClockWise(rotatePointsClockWise(copyPolyomino(prevPolyominos[i]))))));
-
 		for(var j = 0; j < testPolyominos.length; j++) {
 			testPolyominos[j] = normalizePolyomino(testPolyominos[j]);
 			newPolyominos = checkAndAddNewPiece(testPolyominos[j], newPolyominos);
 		}
 	}
-
 	var end_time = Date.now();
-	console.log(num, '-omino took: ', end_time - start_time, 'milliseconds');
+	//console.log(num, '-omino took: ', end_time - start_time, 'milliseconds');
 	return newPolyominos;
 }
-
 /**
 Creates all possible pieces generatable from the passed in (n-1) polyomino
 and determines if that piece already exists in the newPolyominos array and 
 updates the newPolyominos array with the freshly generated piece.
 */
 var checkAndAddNewPiece = function(polycopy, newPolyominos) {
-	
 	for(var j = 0; j < polycopy.pointArray.length; j++) {
 			for(var k = 0 ; k < 2; k++) {
 				if(k === 0) {
@@ -141,11 +115,8 @@ var checkAndAddNewPiece = function(polycopy, newPolyominos) {
 				}
 			}
 		}
-
 	return newPolyominos;
 }
-
-
 /*------------------------------------------------------
                    PIECE MUTATORS
 --------------------------------------------------------
@@ -155,56 +126,41 @@ returns the array after performing a matrix rotation on the
 set of points that are passed in. 
 */
 var rotatePointsClockWise = function(piece) {
-
 	var i;
-
 	for(i = 0; i < piece.pointArray.length; i++) {
 		piece.pointArray[i] = Point(piece.pointArray[i].y, -piece.pointArray[i].x);
 	}
-
 	return piece;
 }
-
 /** Takes a list of points that represent a polyomino. Performs
 the matrix vertical flip operation on each one of the points and 
 returns an array of points. 
 */
 var flipPointsVertically = function(piece) {
-	
 	var i;
-
 	for(i = 0; i < piece.pointArray.length; i++) {
 		piece.pointArray[i] = Point(-piece.pointArray[i].x, piece.pointArray[i].y);
 	}
-
 	return piece;
 }
-
 /** Takes a list of points that represent a polyomino. Performs
 the matrix horizontal flip operation on each one of the poitns and
 returns an array of points. 
 */
 var flipPointsHorizontally = function(piece) {
-
 	var i;
-
 	for(i = 0; i < piece.pointArray.length; i++) {
 		piece.pointArray[i] = Point(piece.pointArray[i].x, -piece.pointArray[i].y);
 	}
-
 	return piece;
 }
-
-
 /*--------------------------------------------------------
 *					PIECE HELPERS 
 ---------------------------------------------------------*/
-
 /*
 Returns a boolean indicating whether two pieces are equal.
 */
 var pieceEquals = function(pieceA, pieceB) {
-
 	var pieceARotated = rotatePointsClockWise(copyPolyomino(pieceA)),
 	pieceARotatedTwice = rotatePointsClockWise(copyPolyomino(pieceARotated)),
 	pieceARotatedThrice = rotatePointsClockWise(copyPolyomino(pieceARotatedTwice)),
@@ -229,8 +185,6 @@ var pieceEquals = function(pieceA, pieceB) {
 	pointArrayAR2FH = normalizePoints(pieceAR2FH.pointArray),
 	pointArrayAR3FV = normalizePoints(pieceAR3FV.pointArray),
 	pointArrayAR3FH = normalizePoints(pieceAR3FH.pointArray);
-
-
 	return  samePointArray(pointArrayA, pointArrayB)
 			||
 			samePointArray(pointArrayARotated, pointArrayB)
@@ -255,7 +209,6 @@ var pieceEquals = function(pieceA, pieceB) {
 			||
 			samePointArray(pointArrayAR3FH, pointArrayB);
 }
-
 //determines if a point array contains the point.
 var pointArrayContains = function (point, pointArray) {
 	for(var i = 0; i < pointArray.length; i++) {
@@ -263,40 +216,31 @@ var pointArrayContains = function (point, pointArray) {
 			return true;
 		}
 	}
-
 	return false;
 }
-
 //determines if two points are equal.
 var pointsEqual = function(pointA, pointB) {
 	return pointA.x === pointB.x && pointA.y === pointB.y;
 }
-
 //create a new copy of the polyomino
 var copyPolyomino = function(polyomino) {
 	var array = copyPointArray(polyomino.pointArray);
 	var numArray = [];
-
 	for(var i = 0; i < array.length; i++) {
 		numArray.push(array[i].x);
 		numArray.push(array[i].y);
 	}
-
 	var copiedPiece = PieceSpecial(numArray);
-
 	return copiedPiece;
 }
-
 //create a new copy of the point array
 var copyPointArray = function(pointArray) {
 	var newArray = [];
 	for(var i = 0; i < pointArray.length; i++) {
 		newArray.push(Point(pointArray[i].x, pointArray[i].y));
 	}
-
 	return newArray;
 }
-
 //determines if piece array contains the given piece.
 var PieceContains = function(Piece, pieceArray) {
 	for(var i = 0; i < pieceArray.length; i++) {
@@ -304,10 +248,8 @@ var PieceContains = function(Piece, pieceArray) {
 			return true;
 		}
 	}
-
 	return false;
 }
-
 //check if the two point arrays are the same
 var samePointArray = function(pointArrayA, pointArrayB) {
 	//they can't be equal if they are different sizes!
@@ -315,54 +257,299 @@ var samePointArray = function(pointArrayA, pointArrayB) {
 		alert("You stupid! The pieces aren't even the same kind!")
 		return false;
 	}
-
 	for(var i = 0; i < pointArrayA.length; i++) {
 		if(!pointArrayContains(pointArrayA[i], pointArrayB)) {
 			return false;
 		}
 	}
-
 	return true;
-
 }
-
 //make sure there's no polyomino that's going out of the grid
 var normalizePoints = function(pointArray) {
 	var minX = Number.POSITIVE_INFINITY,
 	minY = Number.POSITIVE_INFINITY;
-
 	for(var i = 0; i < pointArray.length; i++) {
 		if(pointArray[i].x < minX) {
 			minX = pointArray[i].x;
 		}
-
 		if(pointArray[i].y < minY) {
 			minY = pointArray[i].y;
 		}
 	}
-
 	if(minX < 0) {
 		for(var i = 0; i < pointArray.length; i++) {
 			pointArray[i].x += (-minX);
 		}
 	}
-
 	if(minY < 0) {
 		for(var i = 0; i < pointArray.length; i++) {
 			pointArray[i].y += (-minY);
 		}
 	}
-
 	return pointArray;
 }
-
 //normalize the polyomino and return a new piece.
 var normalizePolyomino = function(piece) {
 	return {
 		pointArray: normalizePoints(piece.pointArray)
 	}
 }
-
 return this;
+}
+)())
 
-};
+polyominoGeneratorFixed = ((function() {
+/**----------------------------------------------------------
+				MODEL ELEMENT CONSTRUCTORS
+------------------------------------------------------------*/
+/** Object factory that creates a point and returns it.
+*/
+var Point = function(x, y) {
+	return {
+		x: x,
+		y: y
+	};
+}
+/** Object factory that creates a list of points that when
+together represent a piece. It returns a point array which is 
+an array of points. 
+*/
+var Piece = function() {
+	var i;
+	//a piece has an array of points.
+	var pointArray = [];
+	for(i = 0; i < arguments.length; i += 2) {
+		pointArray[i/2] = Point(arguments[i], arguments[i+1]); 
+	}
+	return {
+		pointArray: pointArray
+	};
+}
+ var PieceSpecial = function(array) {
+ 	var pointArray = [];
+ 	for(var i = 0; i < array.length; i += 2) {
+ 		pointArray.push(Point(array[i], array[i+1]));
+ 	}
+ 	return {
+ 		pointArray: pointArray
+ 	};
+ }
+/*------------------------------------------------------------
+					ALGORITHMIC OPERATIONS
+------------------------------------------------------------*/
+/** This is the main function that is called
+at the highest level. It calls several of the helper
+functions in the program in order to generate the desired
+array of Hexominos.
+*/
+this.returnPolyominos = function (num) {
+	return generatePolyominos(Number(num));
+}
+/** This is the actual function that calls around everything. 
+It is the "powerhouse of the cell" lol.
+It returns an array of pieces. 
+*/
+var generatePolyominos = function(num) {
+	//var start_time = Date.now();
+	//base case for the recursive function
+	if(num <= 1) {
+		var pieceArray = [];
+		pieceArray[0] = Piece(0, 0);
+		//var end_time = Date.now();
+		//console.log(num, '-omino took: ', end_time - start_time, 'milliseconds');
+		return pieceArray; 
+	}
+	var newPolyominos = [],
+	prevPolyominos = generatePolyominos(num - 1); //recursive call here!!
+	for(var prevpoly of prevPolyominos) {
+		((function(prevpoly){
+			var R0 = copyPolyomino(prevpoly),
+					R1 = rotatePointsClockWise(copyPolyomino(R0)),
+					R2 = rotatePointsClockWise(copyPolyomino(R1)),
+					R3 = rotatePointsClockWise(copyPolyomino(R2));
+			return [
+				R0,
+				R1,
+				R2,
+				R3,
+				flipPointsVertically(copyPolyomino(R0)),
+				flipPointsVertically(copyPolyomino(R1)),
+				flipPointsVertically(copyPolyomino(R2)),
+				flipPointsVertically(copyPolyomino(R3)),
+				//flipPointsHorizontally(copyPolyomino(R0)),
+				//flipPointsHorizontally(copyPolyomino(R1)),
+				//flipPointsHorizontally(copyPolyomino(R2)),
+				//flipPointsHorizontally(copyPolyomino(R3)),
+			];
+		})(prevpoly)).forEach(function(testPoly){console.log(newPolyominos)
+			newPolyominos=checkAndAddNewPiece(normalizePolyomino(testPoly), newPolyominos);
+		});
+	}console.log(newPolyominos)
+	//var end_time = Date.now();
+	//console.log(num, '-omino took: ', end_time - start_time, 'milliseconds');
+	return newPolyominos;
+}
+/**
+Creates all possible pieces generatable from the passed in (n-1) polyomino
+and determines if that piece already exists in the newPolyominos array and 
+updates the newPolyominos array with the freshly generated piece.
+*/
+var checkAndAddNewPiece = function(polycopy, newPolyominos) {
+	for(var j = 0; j < polycopy.pointArray.length; j++) {
+			for(var k = 0 ; k < 2; k++) {
+				if(k === 0) {
+					var newpoint = Point(polycopy.pointArray[j].x + 1, polycopy.pointArray[j].y);
+					if(pointArrayContains(newpoint, polycopy.pointArray)) continue;
+					var newPointArray = copyPointArray(polycopy.pointArray);
+					newPointArray.push(newpoint);
+					var newPiece = {
+						pointArray: newPointArray
+					};
+					if(!PieceContains(newPiece, newPolyominos)) {
+						newPolyominos.push(newPiece);
+					}
+				} else {
+					var newpoint = Point(polycopy.pointArray[j].x, polycopy.pointArray[j].y + 1);
+					if(pointArrayContains(newpoint, polycopy.pointArray)) continue;
+					var newPointArray = copyPointArray(polycopy.pointArray);
+					newPointArray.push(newpoint);
+					var newPiece = {
+						pointArray: newPointArray
+					};
+					if(!PieceContains(newPiece, newPolyominos)) {
+						newPolyominos.push(newPiece);
+					}
+				}
+			}
+		}
+	return newPolyominos;
+}
+/*------------------------------------------------------
+                   PIECE MUTATORS
+--------------------------------------------------------
+*/
+/** Takes a list of points that represent a polyomino and 
+returns the array after performing a matrix rotation on the 
+set of points that are passed in. 
+*/
+var rotatePointsClockWise = function(piece) {
+	var i;
+	for(i = 0; i < piece.pointArray.length; i++) {
+		piece.pointArray[i] = Point(piece.pointArray[i].y, -piece.pointArray[i].x);
+	}
+	return piece;
+}
+/** Takes a list of points that represent a polyomino. Performs
+the matrix vertical flip operation on each one of the points and 
+returns an array of points. 
+*/
+var flipPointsVertically = function(piece) {
+	var i;
+	for(i = 0; i < piece.pointArray.length; i++) {
+		piece.pointArray[i] = Point(-piece.pointArray[i].x, piece.pointArray[i].y);
+	}
+	return piece;
+}
+/** Takes a list of points that represent a polyomino. Performs
+the matrix horizontal flip operation on each one of the poitns and
+returns an array of points. 
+*/
+var flipPointsHorizontally = function(piece) {
+	var i;
+	for(i = 0; i < piece.pointArray.length; i++) {
+		piece.pointArray[i] = Point(piece.pointArray[i].x, -piece.pointArray[i].y);
+	}
+	return piece;
+}
+/*--------------------------------------------------------
+*					PIECE HELPERS 
+---------------------------------------------------------*/
+/*
+Returns a boolean indicating whether two pieces are equal.
+*/
+var pieceEquals = function(pieceA, pieceB) {
+	var pointArrayA = pieceA.pointArray,
+			pointArrayB = pieceB.pointArray;
+	return  samePointArray(pointArrayA, pointArrayB);
+}
+//determines if a point array contains the point.
+var pointArrayContains = function (point, pointArray) {
+	for(var i = 0; i < pointArray.length; i++) {
+		if(pointsEqual(point, pointArray[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+//determines if two points are equal.
+var pointsEqual = function(pointA, pointB) {
+	return pointA.x === pointB.x && pointA.y === pointB.y;
+}
+//create a new copy of the polyomino
+var copyPolyomino = function(polyomino) {if(polyomino==null)debugger
+	return JSON.parse(JSON.stringify(polyomino));
+}
+//create a new copy of the point array
+var copyPointArray = function(pointArray) {
+	var newArray = [];
+	for(var i = 0; i < pointArray.length; i++) {
+		newArray.push(Point(pointArray[i].x, pointArray[i].y));
+	}
+	return newArray;
+}
+//determines if piece array contains the given piece.
+var PieceContains = function(Piece, pieceArray) {
+	for(var i = 0; i < pieceArray.length; i++) {
+		if(pieceEquals(Piece, pieceArray[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+//check if the two point arrays are the same
+var samePointArray = function(pointArrayA, pointArrayB) {
+	//they can't be equal if they are different sizes!
+	if (pointArrayA.length !== pointArrayB.length) {
+		alert("You stupid! The pieces aren't even the same kind!")
+		return false;
+	}
+	for(var i = 0; i < pointArrayA.length; i++) {
+		if(!pointArrayContains(pointArrayA[i], pointArrayB)) {
+			return false;
+		}
+	}
+	return true;
+}
+//make sure there's no polyomino that's going out of the grid
+var normalizePoints = function(pointArray) {
+	var minX = Number.POSITIVE_INFINITY,
+	minY = Number.POSITIVE_INFINITY;
+	for(var i = 0; i < pointArray.length; i++) {
+		if(pointArray[i].x < minX) {
+			minX = pointArray[i].x;
+		}
+		if(pointArray[i].y < minY) {
+			minY = pointArray[i].y;
+		}
+	}
+	if(minX < 0) {
+		for(var i = 0; i < pointArray.length; i++) {
+			pointArray[i].x += (-minX);
+		}
+	}
+	if(minY < 0) {
+		for(var i = 0; i < pointArray.length; i++) {
+			pointArray[i].y += (-minY);
+		}
+	}
+	return pointArray;
+}
+//normalize the polyomino and return a new piece.
+var normalizePolyomino = function(piece) {
+	return {
+		pointArray: normalizePoints(piece.pointArray)
+	}
+}
+return this;
+}
+)())
--- JS/renderPolyomino.js
+++ JS/renderPolyomino.js
@@ -1,3 +1,9 @@
+var setattr = function(o,n,v){
+	var a=document.createAttribute(n);
+	a.value=v;
+	o.attributes.setNamedItem(a)
+	return o.attributes.getNamedItem(a)!=null;
+}
 /**
 This is the master function that you can call by passing in a d3 selected
 svg element on the HTML site. 
@@ -7,57 +13,44 @@ works now. Should be pretty straightforward to extend in the future. I admit,
 this "library" can be improved drastically.
 */
 renderPolyomino = function(chart) {
-
-  /**
-  * This is the function responsible for rendering a piece.
-  * Its parameters are:
-  *   Piece: The piece to be rendered.
-  *   startX: The top left X position to render the anchor (0, 0)
-  *   startY: The top left Y position to render the anchor (0, 0)
-  *   height: The height of each square to be rendered.
-  *   width: The width of each square to be rendered.
-  */
-  function renderPiece(Piece, startX, startY, height, width) {
-
-    var group = chart.append("g");
-
-  	var pieceView = group.selectAll("rect")
-      .data(Piece.pointArray)
-      .enter()
-      .append("rect")
-  		.attr("fill", "white")
-  		.attr("stroke", "black")
-  		.attr("x", function(d) {
-  			return startX + (d.x * width);
-  		})
-  		.attr("y", function(d) {
-  			return startY + (d.y * height);
-  		})
-  		.attr("height", height)
-  		.attr("width", width);
-
-  }
-
-  //loops through all pieces and prints each polyomino
-  this.renderAllPieces = function(polyOminos, startX, startY, squareHeight, squareWidth) {
-
-    var n = polyOminos[0].pointArray.length;
-
-    var piecesPerRow = Math.floor(window.innerWidth / (n * squareWidth));
-
-    //set the width and height
-  	chart.attr("width", window.innerWidth)
-  	.attr("height", (polyOminos.length/piecesPerRow + 2) * squareHeight * n + 200);
-
-    for(var i = 0; i < polyOminos.length; i++) {
-
-  	var polyominoStartX = startX + (i % piecesPerRow) * n * squareWidth,
-  	polyominoStartY = startY + Math.floor(i / piecesPerRow) * n * squareHeight;
-  	
-    renderPiece(polyOminos[i], polyominoStartX, polyominoStartY, squareHeight, squareWidth);
-    }
-
-  }
-
-  return this;
+	var obj_g = document.createElementNS("http://www.w3.org/2000/svg","g");
+	var obj_rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
+	obj_rect.style=("fill: white; stroke: black; stroke-width: 2px");
+	obj_rect.innerHTML="X";
+	/**
+	* This is the function responsible for rendering a piece.
+	* Its parameters are:
+	*   Piece: The piece to be rendered.
+	*   startX: The top left X position to render the anchor (0, 0)
+	*   startY: The top left Y position to render the anchor (0, 0)
+	*   height: The height of each square to be rendered.
+	*   width: The width of each square to be rendered.
+	*/
+	function renderPiece(Piece, startX, startY, squareSizePx) {
+		var group = obj_g.cloneNode(true);
+		chart.appendChild(group);
+		for(var pts of Piece.pointArray){
+			var rct = obj_rect.cloneNode(true);
+			setattr(rct, "x", (startX + (pts.x * squareSizePx)));
+			setattr(rct, "y", (startY + (pts.y * squareSizePx)));
+			group.appendChild(rct);
+		}
+	}
+	//loops through all pieces and prints each polyomino
+	this.renderAllPieces = function(polyOminos, startX, startY, squareSizePx) {
+		setattr(obj_rect, "height", squareSizePx);
+		setattr(obj_rect, "width", squareSizePx);
+		var n = polyOminos[0].pointArray.length;
+		var piecesPerRow = Math.floor(window.innerWidth / (n * squareSizePx));
+		//set the width and height
+		setattr(chart, "width", window.innerWidth);
+		setattr(chart, "height", (polyOminos.length/piecesPerRow + 2) * squareSizePx * n + 200);
+		setattr(chart,"viewBox","0 0");
+		for(var i = 0; i < polyOminos.length; i++) {
+			var polyominoStartX = startX + (i % piecesPerRow) * n * squareSizePx,
+			polyominoStartY = startY + Math.floor(i / piecesPerRow) * n * squareSizePx;
+			renderPiece(polyOminos[i], polyominoStartX, polyominoStartY, squareSizePx);
+		}
+	}
+	return this;
 };
